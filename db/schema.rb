012256212fd159e57e74507a9459d42bd93db0d5
Steven Ng <steven@nomadicoder.com>
2009-12-01 00:20:34 -0800

    Add user authentication and database design

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091022190306) do
+ActiveRecord::Schema.define(:version => 20091109192005) do
 
   create_table "runners", :force => true do |t|
     t.integer  "stage_id"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(:version => 20091022190306) do
   create_table "teams", :force => true do |t|
     t.integer  "number"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
