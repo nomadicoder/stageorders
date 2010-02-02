914e6823a0a1f125a244fc2f4a6643f222f93daa
Steven Ng <steven@nomadicoder.com>
2010-02-02 10:47:20 -0800

    Fixes ordering problem caused by linking runner directly to a stage_id and mistakenly using the stage_id as the order column.

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -3,8 +3,24 @@ class Runner < ActiveRecord::Base
   belongs_to :team
   belongs_to :runner_status_code
   
+  def runner_status
+    runner_status_code.short_code if runner_status_code
+  end
+  
+  def runner_status=(runner_status)
+    self.runner_status_code = RunnerStatusCode.find_by_short_code(runner_status) unless runner_status.blank?
+  end
+  
+  def stage_number
+    stage.number if stage
+  end
+  
+  def stage_number=(number)
+    self.stage = Stage.find_by_number(number) unless number.blank?
+  end
+  
   def self.find_all_runners
-    find(:all, :order => "stage_id")
+    find(:all, :joins => :stage, :order => "stages.number")
   end
   
   def self.find_all_runners_for_team (team_id)
