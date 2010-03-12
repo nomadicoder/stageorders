f3e4f9c23c915903ca17dccd920310930f725533
Steven Ng <steven@nomadicoder.com>
2010-03-11 16:23:41 -0800

    Started time validation.  Still not working. Status page shows status by team with team selector.

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,8 +1,25 @@
 class StatusController < ApplicationController
   def index
     stage_collection = Stage.find_all_stages
-    @runners = Runner.find_all_runners
+    if session[:current_team_id].nil?
+      short_name = Team.find(:first).short_name
+    else
+      team = Team.find(session[:current_team_id]) unless session[:current_team_id].nil?
+      short_name = team.short_name
+    end
+    @runners = Runner.find_all_runners_for_team_code(short_name)
+    #@support_unit = SupportUnits.find_all_support_units
     @stages = stage_collection
+  end
+  
+  def change_team
+    if params[:team][:team_id].nil?
+      session[:current_team_id] = Team.find(:first).id
+    else
+      session[:current_team_id] = params[:team][:team_id]
+    end
+    @team = Team.find(session[:current_team_id])
+    redirect_to :action => :index
   end
 
   def update
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,9 @@ class Team < ActiveRecord::Base
   has_many :runners
   has_many :support_units
   has_many :stage_status
+
+  def self.find_by_short_code (short_code)
+    find(:first, :conditions => "short_name = '#{short_code}'")
+  end
+
 end
