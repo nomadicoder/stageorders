29714fc08b6244b5a73d9f250e5fac784bf51431
Steven Ng <steven@nomadicoder.com>
2010-03-12 14:41:42 -0800

    Debug and refactor Status Team Selection.

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,19 +1,18 @@
 class StatusController < ApplicationController
   def index
     stage_collection = Stage.find_all_stages
-    if session[:current_team_id].nil?
+    if session[:current_team_id].nil? || session[:current_team_id].blank?
       team = Team.find(:first)
     else
-      team = Team.find(session[:current_team_id]) unless session[:current_team_id].nil?
+      team = Team.find(session[:current_team_id])
     end
-    short_name = team.short_name
-    @runners = Runner.find_all_runners_for_team_code(short_name)
-    #@support_unit = SupportUnits.find_all_support_units
+    @team_name = team.name
+    @runners = Runner.find_all_runners_for_team_code(team.short_name)
     @stages = stage_collection
   end
   
   def change_team
-    if params[:team][:team_id].nil?
+    if params[:team][:team_id].nil? || params[:team][:team_id].blank?
       session[:current_team_id] = Team.find(:first).id
     else
       session[:current_team_id] = params[:team][:team_id]
