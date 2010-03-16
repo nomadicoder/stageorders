e25ad506e8cc7abe2c7756af06f71a2ab3f56e05
Steven Ng <steven@nomadicoder.com>
2010-03-16 14:09:03 -0700

    Add team filtering to results page. Add team name to top of results and state statuses pages. Change some runner times in test fixtures to verify filter and team calculations work.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,27 @@
 class ResultsController < ApplicationController
   def index
-    @runners = Runner.find_all_runners
+    if session[:current_team_id].nil? || session[:current_team_id].blank?
+      team = Team.find(:first)
+    else
+      team = Team.find(session[:current_team_id])
+    end
+    @team_name = team.name
+    @runners = Runner.find(:all, :joins => [:stage], :conditions => {:team_id => team.id}, :order => "stages.number")
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @stage_statuses }
+    end
+  end
+
+  def change_team
+    if params[:team][:team_id].nil? || params[:team][:team_id].blank?
+      session[:current_team_id] = Team.find(:first).id
+    else
+      session[:current_team_id] = params[:team][:team_id]
+    end
+    @team = Team.find(session[:current_team_id])
+    redirect_to :action => :index
   end
 
   def authorize
