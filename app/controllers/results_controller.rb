f82987bf0ab8bc7f316db5715cb11869c27d8f47
Steven Ng <steven@nomadicoder.com>
2010-04-15 06:01:19 -0700

    Move stage results calculations out of view and into model

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,16 +7,21 @@ class ResultsController < ApplicationController
     else
       team = Team.find(session[:current_team_id])
     end
-    @team_rec = team
     @start_time = team.start_time
     @team_name = team.name
     @team_id = team.id
     @runners = Runner.find(:all, :joins => [:stage], :conditions => {:team_id => team.id}, :order => "stages.number")
     
+    @results = Results.new (team)
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @stage_statuses }
     end 
+  end
+  
+  def calculate_stage_results (stage_number)
+    @results.calculate_stage_results
   end
 
   def change_team
---
+++ app/models/results.rb
@@ -0,0 +1,60 @@
+class Results
+  def initialize (team)
+    @total_minutes = 0.0 
+    @total_estimated_minutes = 0.0 
+    @total_off_pace = 0.0 
+    @time_zero = 946684800 
+
+    @team = team
+    @stage_start = @team.start_time 
+    @stage_finish = @stage_start
+  end
+  
+  def time_zero
+    @time_zero
+  end
+  
+  def total_minutes
+    @total_minutes
+  end
+  
+  def total_estimated_minutes
+    @total_estimated_minutes
+  end
+  
+  def total_off_pace
+    @total_off_pace
+  end
+  
+  def stage_start
+    @stage_start
+  end
+  
+  def stage_finish
+    @stage_finish
+  end
+  
+  def estimated_minutes
+    @estimated_minutes
+  end
+  
+  def calculate_stage_results (runner)
+    @stage_start = @stage_finish
+    @estimated_minutes = runner.stage.miles * (runner.estimated_pace.min + runner.estimated_pace.sec/60.0)
+    @total_estimated_minutes = @total_estimated_minutes + @estimated_minutes
+    
+    if runner.actual_time.to_i != @time_zero
+      @actual_minutes = time_to_minutes(runner.actual_time)
+      @total_minutes = @total_minutes + @actual_minutes
+      @off_pace = @actual_minutes - @estimated_minutes
+      @total_off_pace = total_off_pace + off_pace
+    end
+    
+    if runner.actual_time.to_i != @time_zero
+        @stage_time = time_to_minutes(runner.actual_time)
+    else
+        @stage_time = @estimated_minutes
+    end
+    @stage_finish += (60 * @stage_time)
+  end
+end
