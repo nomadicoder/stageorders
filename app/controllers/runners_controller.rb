5323f039e7df7627fa059bc987ee144cf1da5646
Steven Ng <steven@nomadicoder.com>
2011-04-16 16:39:07 -0700

    Add hour to time in title

--- app/controllers/runners_controller.rb
+++ app/controllers/runners_controller.rb
@@ -105,7 +105,7 @@ class RunnersController < ApplicationController
 
     title = runner.team.name +
             " Stage " + runner.stage.number.to_s +
-            " Result (" + runner.actual_time.strftime("%M:%S") + ")"
+            " Result (" + runner.actual_time.strftime("%H:%M:%S") + ")"
     description = runner.team.name + " completed the " +
                   runner.stage.miles.to_s + " mile of Stage " +
                   runner.stage.number.to_s + " with a time of " +
