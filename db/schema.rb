8fc9f29b231b59ce0b4b4421bbff72d6ad599c34
Steven Ng <steven@nomadicoder.com>
2017-03-12 20:08:46 -0400

    Update Rails

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,35 +12,35 @@
 
 ActiveRecord::Schema.define(version: 20130410035054) do
 
-  create_table "blogs", force: true do |t|
+  create_table "blogs", force: :cascade do |t|
     t.integer  "team_id"
-    t.string   "host_url"
-    t.string   "access_path"
-    t.string   "blog_url"
-    t.string   "results_url"
-    t.string   "access_url"
-    t.string   "username"
-    t.string   "password"
-    t.string   "blog_number"
+    t.string   "host_url",            limit: 255
+    t.string   "access_path",         limit: 255
+    t.string   "blog_url",            limit: 255
+    t.string   "results_url",         limit: 255
+    t.string   "access_url",          limit: 255
+    t.string   "username",            limit: 255
+    t.string   "password",            limit: 255
+    t.string   "blog_number",         limit: 255
     t.integer  "results_post_number"
-    t.string   "category"
+    t.string   "category",            limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "runner_status_codes", force: true do |t|
+  create_table "runner_status_codes", force: :cascade do |t|
     t.float    "sequence"
-    t.string   "short_code"
+    t.string   "short_code",  limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "runners", force: true do |t|
+  create_table "runners", force: :cascade do |t|
     t.integer  "stage_id"
     t.integer  "team_id"
     t.integer  "runner_status_code_id"
-    t.string   "name"
+    t.string   "name",                  limit: 255
     t.time     "estimated_pace"
     t.boolean  "completed"
     t.time     "actual_time"
@@ -49,9 +48,9 @@ ActiveRecord::Schema.define(version: 20130410035054) do
     t.datetime "updated_at"
   end
 
-  create_table "stage_status_codes", force: true do |t|
-    t.string   "short_code"
-    t.string   "description"
+  create_table "stage_status_codes", force: :cascade do |t|
+    t.string   "short_code",             limit: 255
+    t.string   "description",            limit: 255
     t.float    "sequence"
     t.integer  "runner_status_code_id"
     t.integer  "support_status_code_id"
@@ -59,7 +58,7 @@ ActiveRecord::Schema.define(version: 20130410035054) do
     t.datetime "updated_at"
   end
 
-  create_table "stage_statuses", force: true do |t|
+  create_table "stage_statuses", force: :cascade do |t|
     t.integer  "team_id"
     t.integer  "stage_id"
     t.integer  "runner_id"
@@ -71,35 +70,35 @@ ActiveRecord::Schema.define(version: 20130410035054) do
     t.datetime "updated_at"
   end
 
-  create_table "stages", force: true do |t|
+  create_table "stages", force: :cascade do |t|
     t.integer  "number"
-    t.string   "landmark"
+    t.string   "landmark",   limit: 255
     t.float    "miles"
     t.integer  "difficulty"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "support_status_codes", force: true do |t|
+  create_table "support_status_codes", force: :cascade do |t|
     t.float    "sequence"
-    t.string   "short_code"
+    t.string   "short_code",  limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "support_types", force: true do |t|
-    t.string   "short_code"
+  create_table "support_types", force: :cascade do |t|
+    t.string   "short_code",  limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "support_units", force: true do |t|
+  create_table "support_units", force: :cascade do |t|
     t.integer  "team_id"
     t.integer  "support_type_id"
-    t.string   "tac_callsign"
-    t.string   "ham_callsign"
+    t.string   "tac_callsign",           limit: 255
+    t.string   "ham_callsign",           limit: 255
     t.integer  "support_status_code_id"
     t.integer  "current_stage_id"
     t.float    "location_lat"
@@ -108,33 +107,32 @@ ActiveRecord::Schema.define(version: 20130410035054) do
     t.datetime "updated_at"
   end
 
-  create_table "teams", force: true do |t|
+  create_table "teams", force: :cascade do |t|
     t.integer  "number"
-    t.string   "name"
-    t.string   "short_name"
+    t.string   "name",       limit: 255
+    t.string   "short_name", limit: 255
     t.time     "start_time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  default: false
-    t.boolean  "staff",                  default: false
+    t.boolean  "admin",                              default: false
+    t.boolean  "staff",                              default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
